50f51f66b1f6e07370d9a4594dd484050f2928d1
eLruloSz <130678389+eLruloSz@users.noreply.github.com>
2025-01-22 18:58:29 +0000

    aaaa

--- main.cpp
+++ main.cpp
@@ -10,15 +10,17 @@ using namespace std;
 class Nodo{
    public:
     pair<char,int> nodo;
-    vector<Nodo*> sig;
-    Nodo(char letra, int peso) : nodo(letra,peso){}
+    vector<Nodo*> vecinos;
+    Nodo(char letra, int peso){
+        nodo = make_pair(letra,peso);
+    }
     
 };
 
 
-void imprimirVect(vector<int> v){
+void imprimirVect(vector<Nodo*> v){
     for(int i = 0; i < v.size(); i++){
-        cout<<v[i]<<" ";
+        cout<<v[i]->nodo.first<<" ";
     }
 }
 
@@ -27,18 +29,21 @@ vector<int> dijkstra(vector<vector<int>> &matriz){
     vector<int> caminoCorto(n,99999);
     vector<bool> nodos(n,0);
     caminoCorto[0] = 0;
-    
+    vector<Nodo*> arbol;
     for(int i = 0; i < n; i++){
         for(int j = 1; j < n; j++){
             if(matriz[i][j] != 0){
                 int peso = matriz[i][j];
+                nodos[j] = 1;
                 if(caminoCorto[i] + peso < caminoCorto[j]){
                     caminoCorto[j] = caminoCorto[i] + peso;
-
+                    Nodo* n = new Nodo(i+65,caminoCorto[i]);
+                    nodos.push_back(n);
                 }
             }      
         }
     }
+
     return caminoCorto;
 }
 
@@ -46,10 +51,10 @@ vector<int> dijkstra(vector<vector<int>> &matriz){
 
 void imprimirNodos(queue<int> nodo){
     queue<char> nodos;
-    cout<<"Los nodos presentes son: "<<endl;
+    cout<<"Los nodos presentes son: ";
     while(!nodo.empty()){
         nodos.push(nodo.front() + 65);
-        nodo.pop();
+        
     }
     while(!nodos.empty()){
         cout<<nodos.front()<<" ";
@@ -78,11 +83,13 @@ int main(){
     string primeraLinea;
     getline(archivo,primeraLinea);
     int n = stoi(primeraLinea);
+    
 
     vector<vector<int>> matrizAdy(n,vector<int>(n));
     int contador = 0;
     string linea;
     queue<int> nodos;
+
     for(int i = 0; i < n; i++){
         getline(archivo,linea);
         stringstream ss(linea);
@@ -107,7 +114,7 @@ int main(){
     cout<<endl;
     vector<int> caminoCorto = dijkstra(matrizAdy);
     char nodoFin;
-    cout<<"Ponga el nodo a llegar: "<<endl;
+    cout<<"Ponga el nodo a llegar: ";
     cin>>nodoFin; 
 
     int nodoFinInt = nodoFin - 65;
@@ -116,7 +123,7 @@ int main(){
             cout<<"El valor total del camino más corto: "<<caminoCorto[i]<<endl;
         }
     } 
-
+    
 
     return 0;
 }
